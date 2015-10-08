9f892376c2735342f2b02e9d3f039f58031c3279
henning96 <ger.henning96@gmail.com>
2015-10-08 15:10:39 +0200

    config is now generated at install

--- postinstall.js
+++ postinstall.js
@@ -1,20 +1,25 @@
 var fs = require('fs');
-var config = require('./config');
 var path = require('path');
-var child_process = require('child_process');
+var childProcess = require('child_process');
 var Q = require('q');
-var spawn = child_process.spawn;
-var location = path.join(__dirname, config.key.filename);
-var stat = Q.nfbind(fs.stat);
 
-Q.all([
-    stat(location),
-    stat(location + ".pub")
-])
-.then(function(){
-    console.log("SSH-Key does already exist");
-}, function(){
-    var sshKeygen = spawn('ssh-keygen', [
+var stat = Q.nfbind(fs.stat);
+var os = require('os');
+
+var config = {
+    "cartridge": "nodejs-0.10",
+    "key": {
+        "filename": "openshift_rsa",
+        "name": "cloudify" + "_" + os.hostname(),
+        "comment": "created by CloudifyToMM (ToMM Apps GmbH)",
+        "password": "cloudify"
+    }
+};
+
+var location = path.join(__dirname, config.key.filename);
+
+if(!fs.existsSync(location) || !fs.existsSync(location + ".pub")){
+    var sshKeygen = childProcess.spawn('ssh-keygen', [
         '-t','rsa',
         '-C', config.key.comment,
         '-f', location,
@@ -28,4 +33,14 @@ Q.all([
     sshKeygen.stderr.on('data', function(data){
         console.error(data.toString());
     });
-});
+} else {
+    console.log("SSH Key does already exist!");
+}
+
+var configPath = path.join(__dirname, "config.json");
+
+if(!fs.existsSync(configPath)){
+    fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(config));
+}
+
+
